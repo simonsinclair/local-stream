d8d461d6a95a4962846dd1633bbc7b3fb3beb26b
Simon Sinclair <simon@sinclair>
2014-10-21 16:04:53 +0100

    Refer to master branch

--- js/main.js
+++ js/main.js
@@ -39,10 +39,6 @@
       Local.numIncludedPartials  = 0;
       Local.awaitingNotification = false;
 
-      $.ajaxSetup({
-        // ...
-      });
-
       // Start
       Local.bindEvts();
       Local.includePartials();
@@ -84,8 +80,8 @@
       });
 
       // Load New updates
-      $('#js-stream-snu', Local.$page).on('tap', Local.loadNewUpdates);
-      $('#js-pop-in', Local.$page).on('tap', Local.loadNewUpdates);
+      $('#js-stream-snu,#js-pop-in', Local.$page).on('tap', Local.loadNewUpdates);
+      // $('#js-pop-in', Local.$page).on('tap', Local.loadNewUpdates);
     },
 
     includePartials: function() {
@@ -131,7 +127,9 @@
       $('#js-pop-in', Local.$page).removeClass('pop-in--visible');
 
       if(e.target.id === 'js-pop-in') {
-        $(document).scrollTop(874);
+        $('html,body').animate({
+          scrollTop: $('#js-stream-header', Local.$page).offset().top
+        }, 750);
       }
 
       Local.insertNewUpdates( afterInsertNewUpdates );
